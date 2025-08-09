3c0fcf90c5380c710fa849bbe39cc4f7b2e16923
Patrick Marck D <patrickmarckdulaca@gmail.com>
2025-08-09 15:20:28 +0800

    Update #10

--- services/email.js
+++ services/email.js
@@ -2,6 +2,7 @@ const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
 const _ = require("lodash");
 const path = require("path");
+const padayon = require("./padayon");
 const testEnv = ["local", "dev", "uat"];
 const isTestMode = testEnv.includes(process.env.NODE_ENV) ? true : false;
 // const subjectAppendedText = isTestMode ? " **FOR TESTING PURPOSES ONLY**" : "";
@@ -28,64 +29,75 @@ module.exports.notify = async (recipient, template, data = {}) => {
   console.log('----2', template)
   console.log('----3', data)
 
-  transporter.use(
-    "compile",
-    hbs({
-      viewEngine: {
-        extname: ".handlebars", // handlebars extension
-        layoutsDir: path.resolve(__dirname, '..', 'assets/templates'), // location of handlebars templates
-        defaultLayout: template, // name of main template
-        cache: false
+  try {
+
+    transporter.use(
+      "compile",
+      hbs({
+        viewEngine: {
+          extname: ".handlebars", // handlebars extension
+          layoutsDir: path.resolve(__dirname, '..', 'assets/templates'), // location of handlebars templates
+          defaultLayout: template, // name of main template
+          cache: false
+        },
+        viewPath: path.resolve(__dirname, '..', 'assets/templates'),
+        extName: '.handlebars'
+      })
+    );
+
+    const mail = {
+      headers: {
+        "X-Priority": "1 (Highest)",
+        "X-MSMail-Priority": "High",
+        Importance: "High"
       },
-      viewPath: path.resolve(__dirname, '..', 'assets/templates'),
-      extName: '.handlebars'
-    })
-  );
 
-  const mail = {
-    headers: {
-      "X-Priority": "1 (Highest)",
-      "X-MSMail-Priority": "High",
-      Importance: "High"
-    },
+      from: `"Kuweba Software Solutions" <${username}>`,
+      to: recipient,
+      subject: data.header,
+      template: template,
+      attachments: [
+        {
+          filename: "email_banner.png",
+          path: process.cwd() + `/assets/images/${data.banner}.png`,
+          cid: "email_banner",
+        },
+        // {
+        //   filename: "facebook-icon.png",
+        //   path: process.cwd() + "/assets/images/icons/facebook-icon.png",
+        //   cid: "facebook-icon",
+        // },
+        // {
+        //   filename: "instagram-icon.png",
+        //   path: process.cwd() + "/assets/images/icons/instagram-icon.png",
+        //   cid: "instagram-icon",
+        // },
+        // {
+        //   filename: "youtube-icon.png",
+        //   path: process.cwd() + "/assets/images/icons/youtube-icon.png",
+        //   cid: "youtube-icon",
+        // },
 
-    from: `"Kuweba Software Solutions" <${username}>`,
-    to: recipient,
-    subject: data.header,
-    template: template,
-    attachments: [
-      {
-        filename: "email_banner.png",
-        path: process.cwd() + `/assets/images/${data.banner}.png`,
-        cid: "email_banner",
+      ],
+      context: {
+        ...data,
       },
-      // {
-      //   filename: "facebook-icon.png",
-      //   path: process.cwd() + "/assets/images/icons/facebook-icon.png",
-      //   cid: "facebook-icon",
-      // },
-      // {
-      //   filename: "instagram-icon.png",
-      //   path: process.cwd() + "/assets/images/icons/instagram-icon.png",
-      //   cid: "instagram-icon",
-      // },
-      // {
-      //   filename: "youtube-icon.png",
-      //   path: process.cwd() + "/assets/images/icons/youtube-icon.png",
-      //   cid: "youtube-icon",
-      // },
+    };
 
-    ],
-    context: {
-      ...data,
-    },
-  };
+    if (_.size(data.attachments)) {
+      mail.attachments.push(...data.attachments)
+    }
 
-  if (_.size(data.attachments)) {
-    mail.attachments.push(...data.attachments)
+    const result = await transporter.sendMail(mail);
+    transporter.close();
+    return result;
+  } catch (error) {
+    padayon.ErrorHandler(
+      "Services::Email::Notify",
+      error,
+      req,
+      res
+    );
   }
 
-  const result = await transporter.sendMail(mail);
-  transporter.close();
-  return result;
 };
