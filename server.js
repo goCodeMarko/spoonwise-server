7ad50f8576022a28791c1fc64c4aef334e229409
Patrick Marck D <patrickmarckdulaca@gmail.com>
2026-03-25 06:51:20 +0800

    Update #25

--- server.js
+++ server.js
@@ -135,7 +135,7 @@
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Timezone"],
+
   };
 
   app
